a63bebccb6c9bfd8298d6b15c42573e2ab60b499
Diego Alejandro Delgado Gontes <diegodelgadog1@gmail.com>
2026-04-13 19:47:55 -0600

    dawddwwda

--- index.js
+++ index.js
@@ -2,12 +2,24 @@
 
 import express from 'express';
 import { createServer } from 'node:http';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server, {
+  cors: "http://localhost:5174/",
+});
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world 2</h1>');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected', socket.id);
+  });
 });
 
 
